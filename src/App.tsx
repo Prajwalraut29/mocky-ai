e8fa67a2d6231f95f8088dccc4690490d8890508
Prajwalraut29 <rautprajwal36@gmail.com>
2025-04-04 08:09:15 +0530

    changes : setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <p className="bg-red-400">
+        Click on the Vite and React logos to learn more
+      </p>
+      <Button>click me </Button>
+    </>
+  )
+}
+
+export default App
